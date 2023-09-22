61c04a19bf083501f27dc6dbf40462d10dbd9d7d
Norman Munge <normanmunge@gmail.com>
2023-09-22 07:04:11 +0300

    Adds self signed certs for ssl

--- index.js
+++ index.js
@@ -1,39 +1,68 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const createError = require("http-errors");
+require('dotenv').config();
+const express = require('express');
+const body_parser = require('body-parser');
+const app = express().use(body_parser.json());
+const createError = require('http-errors');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+
 const port = process.env.PORT || 3000;
 
-const welcomeRouter = require("./routes/welcome");
-const responseRouter = require("./routes/responses");
+//routes
+const welcomeRouter = require('./routes/welcome');
+const responseRouter = require('./routes/responses');
 
-app.get("/test", (req, res) => {
-  console.log("THE PROCESS ENV", process.env.RECIPIENT_WAID);
+app.get('/test', (req, res) => {
+  console.log('THE PROCESS ENV', process.env.RECIPIENT_WAID);
   res.sendStatus(200);
   return;
 });
 
-app.use("/welcome", welcomeRouter);
+app.use('/welcome', welcomeRouter);
 
 const verify_token = process.env.VERIFY_TOKEN;
 
-app.get("/responses", (req, res) => {
+app.get('/', (req, res) => {
+  res.send('Welcome to Wekeza');
+  return;
+});
+
+app.post('/responses', async (req, res) => {
+  let body = req.body;
+  console.log('THE INCOMING BODY MESSAGE', body);
+
   if (req.query.token !== verify_token) {
     return res.sendStatus(401); //unauthorized
   }
 
   res.sendStatus(200);
-  console.log("THE REQUEST NOW", req.body);
+  console.log('THE REQUEST NOW', req.body);
   return;
 });
+
+// app.get('/', (req, res) => {
+//   console.log('WELCOME TO WEKEZA');
+//   return;
+// });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
+//ssl
+// const sslOptions = {
+//   key: fs.readFileSync(path.join(__dirname, './https-cert/CA/key.pem')),
+//   cert: fs.readFileSync(path.join(__dirname, './https-cert/CA/cert.pem')),
+// };
+// const sslServer = https.createServer(sslOptions, app);
+// sslServer.listen(port, () => {
+//   console.log(`Secure Server started on port ${port}`);
+// });
+
 app.listen(port, () => {
-  console.log("Server started");
+  console.log(`Secure Server started on port ${port}`);
 });
 
 module.exports = app;
--- routes/responses.js
+++ routes/responses.js
@@ -1,20 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const body_parser = require("body-parser");
-require("dotenv").config();
+const body_parser = require('body-parser');
+require('dotenv').config();
 
 const verify_token = process.env.VERIFY_TOKEN;
 
 router.use(body_parser.json());
 
-router.post("/responses", (req, res) => {
-  console.log("THE REQUEST", verify_token);
+router.post('/responses', (req, res) => {
+  console.log('THE REQUEST', verify_token);
   if (req.query.token !== verify_token) {
     return res.sendStatus(401); //unauthorized
   }
 
   res.sendStatus(200);
-  console.log("THE REQUEST NOW", req.body);
+  console.log('THE REQUEST NOW', req.body);
   return;
 });
 
