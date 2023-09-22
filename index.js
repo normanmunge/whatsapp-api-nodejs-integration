ff664be78121cc78814f6c8ffab4aacfde6fdfa8
Norman Munge <normanmunge@gmail.com>
2023-09-22 05:14:55 +0300

    Tries to add callback url

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const createError = require('http-errors');
+const createError = require("http-errors");
 const port = process.env.PORT || 3000;
 
-const welcomeRouter = require('./routes/welcome');
+const welcomeRouter = require("./routes/welcome");
+const responseRouter = require("./routes/responses");
 
-app.get('/test', (req, res) => {
-  console.log('THE PROCESS ENV', process.env.RECIPIENT_WAID);
+app.get("/test", (req, res) => {
+  console.log("THE PROCESS ENV", process.env.RECIPIENT_WAID);
   res.sendStatus(200);
   return;
 });
 
-app.use('/welcome', welcomeRouter);
+app.use("/welcome", welcomeRouter);
+
+const verify_token = process.env.VERIFY_TOKEN;
+
+app.get("/responses", (req, res) => {
+  if (req.query.token !== verify_token) {
+    return res.sendStatus(401); //unauthorized
+  }
+
+  res.sendStatus(200);
+  console.log("THE REQUEST NOW", req.body);
+  return;
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
+
 app.listen(port, () => {
-  console.log('Server started');
+  console.log("Server started");
 });
 
 module.exports = app;
---
+++ routes/responses.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const body_parser = require("body-parser");
+require("dotenv").config();
+
+const verify_token = process.env.VERIFY_TOKEN;
+
+router.use(body_parser.json());
+
+router.post("/responses", (req, res) => {
+  console.log("THE REQUEST", verify_token);
+  if (req.query.token !== verify_token) {
+    return res.sendStatus(401); //unauthorized
+  }
+
+  res.sendStatus(200);
+  console.log("THE REQUEST NOW", req.body);
+  return;
+});
+
+module.exports = router;
