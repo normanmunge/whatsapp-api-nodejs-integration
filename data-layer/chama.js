3632cbca63a8475f653209ad661d52419f07b93a
Norman Munge <normanmunge@gmail.com>
2023-10-22 03:45:52 +0300

    Adds fetching chama frequency and next recipient details

--- data-layer/chama.js
+++ data-layer/chama.js
@@ -15,6 +15,22 @@ class ChamaDataLayer {
     const chama = await db('chama').where('id', id).first();
     return chama;
   }
+
+  async getChamaContributionFrequency(id) {
+    const frequency = await db('chama_contribution_frequency')
+      .where('chama_id', id)
+      .first();
+    return frequency;
+  }
+
+  async getChamaNextRecipient(id, cycle) {
+    const next_cycle_count = cycle + 1;
+    const recipient = await db('member_cycle_count')
+      .where('chama_id', id)
+      .where('cycle_number', next_cycle_count)
+      .first();
+    return recipient;
+  }
 }
 
 module.exports = new ChamaDataLayer();
